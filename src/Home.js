0817d6682029562aa99569b43191deec8eec8606
ivan verushkin <ivanveruskin86@gmail.com>
2024-10-02 10:01:26 +0300

    Update home page

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { useContext } from "react";
 import { UserContext } from "./context/UserProvider";
+import { Link } from "react-router-dom";
 
 function Home(){
     const {user} = useContext (UserContext);
     return ( 
-        <>
-            {user && user.name && <p>Welcome, {user.name}</p>}
-            {!user.isLoggedIn && <p>Please sign in!</p>}
-        </>
+        <div id='home-container'>
+            <div id='home-text'>
+                <div>
+                    <h1 className="welcome-header"> Welcome to the To Do app</h1>
+                    <p className="welcome-header-low">A place where you can manage your future tasks</p>
+                </div>
+                <div className="home-content">
+                    {user && user.name && <p>Welcome, {user.name}</p>}
+                    {!user.isLoggedIn && <p>Before we are able to start... Please <Link to='/signin'>sign in!</Link></p>}
+                </div>
+            </div>
+        </div>
     );
 }
 
